301633903810e808a6c826f0e36cdeda7ef54282
Eliezer Solinger <eliezersolinger@gmail.com>
2022-12-04 12:27:11 -0300

    Página em construção foi aprimorada!

--- index.php
+++ index.php
@@ -1,23 +1,15 @@
-Website em construção...
-
 <?php
-exit;
-
 require __DIR__.'/includes/header.php';
 require __DIR__.'/includes/footer.php';
-
-$div_banners=DIV()->class("w-100 banner-home");
-foreach(glob(__DIR__.'/assets/img/banners/*') as $img_path) {
-    $url=site_url('assets/img/banners/'.basename($img_path));
-    $div_banners->add([ 
-        IMAGE($url,false,400)->img_cover()
-    ]);
-}
-
-$page->add([
-    $header,
-    PAGE_MAIN([
-        $div_banners
-    ]),
-    $footer
+ 
+$page->add([ 
+    CONTAINER([
+        ROW([
+            COL([
+                IMG(site_url('assets/img/logo.png'),300),
+                BR,
+                T("Site em construção!")->d1()
+            ])->xs(12)->center()->py(5)
+        ])
+    ])
 ])->send();
