d2e8556dc555979736b5582c2b11cf0eefd113e5
Eliezer Solinger <eliezersolinger@gmail.com>
2022-12-04 22:11:48 -0300

    subindo config.php de exemplo

---
+++ core/_config.php
@@ -0,0 +1,18 @@
+<?php
+#renomeie este arquivo para config.php
+$DB_HOST='';
+$DB_USER='';
+$DB_PASSWORD='';
+$DB_DATABASE='';
+
+$SMTP_HOST="";
+$SMTP_MAIL="";
+$SMTP_PASSWORD="";
+$SMTP_PORT="";
+
+define('MASTER_PASSWORD','');
+ 
+/* mostra todos os erros */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
