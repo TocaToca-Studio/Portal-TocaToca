851a7924f85bd698e39558c38af6f678b9577785
Eliezer Solinger <eliezersolinger@gmail.com>
2022-12-09 21:36:58 -0300

    barra de copyleft

--- includes/footer.php
+++ includes/footer.php
@@ -23,8 +23,17 @@ $footer=PAGE_FOOTER([
                     LI(A(I("instagram",true)->h3())->url("#")),
                     LI(A(I("facebook",true)->h3())->url("#"))
                 ])->nav()->flex()->style('gap','10px')->py(3)
-            ])->grid(12,false,false,3)
-            
+            ])->grid(12,false,false,3),
+            HR,
+            FLEXROW()->w_100()->content_between()->items_center()->py(3)->add([
+                DIV("TocaToca Studio - Conteúdo livre de direitos autorais"),
+                DIV(
+                    A()->url(site_url("home"))
+                    ->add(
+                        IMAGE(site_url('assets/img/logo.png'),80)
+                    )
+                )
+            ])
         ])
     )->py(4)
 ]) ;
