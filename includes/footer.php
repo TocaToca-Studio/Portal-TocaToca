acb436651916c4b28825b90a09cafe848ba933d4
Eliezer Solinger <eliezersolinger@gmail.com>
2022-12-11 17:56:36 -0300

    melhorias no rodapé

--- includes/footer.php
+++ includes/footer.php
@@ -24,9 +24,14 @@ $footer=[
                     LI(A(I("instagram",true)->h3())->url("#")),
                     LI(A(I("facebook",true)->h3())->url("#"))
                 ])->nav()->flex()->style('gap','10px')->py(3)
-            ])->grid(12,false,false,3),
-            HR,
-            FLEXROW()->w_100()->content_between()->items_center()->py(3)->add([
+            ])->grid(12,false,false,3)
+        ])
+    )->py(4),
+    DIV()->w_100()->add([
+        CONTAINER([
+            FLEXROW()->w_100()->content_between()->items_center()->py(3)
+            ->style("border-top",'1px solid black')
+            ->add([
                 DIV("TocaToca Studio - Conteúdo livre de direitos autorais"),
                 DIV(
                     A()->url(site_url("home"))
@@ -36,7 +41,7 @@ $footer=[
                 )
             ])
         ])
-    )->py(4)
+    ])->bg_primary()->white()
 ]),
 $modais
 ];
